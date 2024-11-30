98f6ef7bf131b01d7d640c9b7f516d45f0a797fd
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-11-30 06:20:11 +0400

    Handled ingredient addition but in imperative way

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,47 @@
+ import { useState } from "react";
 import "./Main.css";
 
 const Main = () => {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+  const ingredientListItems = ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    addIngredient(newIngredient);
+    event.currentTarget?.reset();
+  }
+
+  function addIngredient(ingredient: unknown) {
+    setIngredients((prevIngredient) => [
+      ...prevIngredient,
+      ingredient as string,
+    ]);
+  }
+
   return (
     <main>
-      <form className="ingredientForm">
+      <form onSubmit={handleSubmit} className="ingredientForm">
         <input
           type="text"
           placeholder="e.g. Oregano"
           aria-label="Add ingedient"
+          name="ingredient"
         />
-        <button type="button">Add ingedient</button>
+        <button>Add ingedient</button>
       </form>
+
+      <ul>{ingredientListItems}</ul>
     </main>
   );
 };
