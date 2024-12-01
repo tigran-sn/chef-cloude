0850b802ef58d69806697c5319406ed97d39fc88
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-01 16:59:07 +0400

    Added conditional renderings: - Render ingredients section, when ingredient list is not empty - Render the section for getting recipes when there is more than 3 recipe in the list

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,7 +2,7 @@
 import "./Main.css";
 
 const Main = () => {
-  const [ingredients, setIngredients] = useState([
+  const [ingredients, setIngredients] = useState<string[]>([
     "Chicken",
     "Oregano",
     "Tomatoes",
@@ -20,13 +20,13 @@ const Main = () => {
     const newIngredient = formData.get("ingredient");
     addIngredient(newIngredient);
     event.currentTarget?.reset();
+    console.log(Object.fromEntries(formData))
   }
 
   function addIngredient(ingredient: unknown) {
-    setIngredients((prevIngredient) => [
-      ...prevIngredient,
-      ingredient as string,
-    ]);
+    if (typeof ingredient === 'string') {
+        setIngredients((prevIngredient) => [...prevIngredient, ingredient]);
+    }
   }
 
   return (
@@ -41,7 +41,17 @@ const Main = () => {
         <button>Add ingedient</button>
       </form>
 
-      <ul>{ingredientListItems}</ul>
+      {!!ingredientListItems.length && <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients__list" aria-live="polite">{ingredientListItems}</ul>
+        {ingredientListItems.length > 3 && <div className="getRecipe__container">
+          <div>
+            <h3>Ready for recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>}
+      </section>}
     </main>
   );
 };
