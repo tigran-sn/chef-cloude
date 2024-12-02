c627c31fa86a137e1062d89b405d57abee6772b5
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-02 21:41:51 +0400

    - Created IngredientsList component - Moved related functionality to the component from Main component

---
+++ src/components/IngredientsList/IngredientsList.tsx
@@ -0,0 +1,31 @@
+const IngredientsList = ({
+  ingredients,
+  toggleRecipeShown,
+}: {
+  ingredients: string[];
+  toggleRecipeShown: () => void;
+}) => {
+  const ingredientListItems = ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ));
+
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients__list" aria-live="polite">
+        {ingredientListItems}
+      </ul>
+      {ingredients.length > 3 && (
+        <div className="getRecipe__container">
+          <div>
+            <h3>Ready for recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={toggleRecipeShown}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default IngredientsList;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Main.css";
 import RecipePlaceholder from "../RecipePlaceholder";
+import IngredientsList from "../IngredientsList";
 
 const Main = () => {
   const [recipeShown, setRecipeShown] = useState(false);
@@ -11,29 +12,25 @@ const Main = () => {
     "heavy cream",
     "pasta",
   ]);
-  const ingredientListItems = ingredients.map((ingredient, index) => (
-    <li key={index}>{ingredient}</li>
-  ));
 
   function handleSubmit(event: {
     preventDefault: () => void;
     currentTarget: HTMLFormElement | undefined;
-  }) {
+  }): void {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
     addIngredient(newIngredient);
     event.currentTarget?.reset();
-    console.log(Object.fromEntries(formData));
   }
 
-  function addIngredient(ingredient: unknown) {
+  function addIngredient(ingredient: unknown): void {
     if (typeof ingredient === "string") {
       setIngredients((prevIngredient) => [...prevIngredient, ingredient]);
     }
   }
 
-  function toggleRecipe() {
+  function toggleRecipeShown(): void {
     setRecipeShown((prevState: boolean) => !prevState);
   }
 
@@ -49,22 +46,11 @@ const Main = () => {
         <button>Add ingedient</button>
       </form>
 
-      {!!ingredientListItems.length && (
-        <section>
-          <h2>Ingredients on hand:</h2>
-          <ul className="ingredients__list" aria-live="polite">
-            {ingredientListItems}
-          </ul>
-          {ingredientListItems.length > 3 && (
-            <div className="getRecipe__container">
-              <div>
-                <h3>Ready for recipe?</h3>
-                <p>Generate a recipe from your list of ingredients.</p>
-              </div>
-              <button onClick={toggleRecipe}>Get a recipe</button>
-            </div>
-          )}
-        </section>
+      {!!ingredients.length && (
+        <IngredientsList
+          ingredients={ingredients}
+          toggleRecipeShown={toggleRecipeShown}
+        />
       )}
       {recipeShown && <RecipePlaceholder />}
     </main>
