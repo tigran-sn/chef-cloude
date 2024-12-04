60469ba59c21a1f4b1cedb8b3b18c2b4af28af59
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-04 09:25:48 +0400

    - Installed react-markdown - Added styles

--- src/components/ClaudeRecipe/ClaudeRecipe.tsx
+++ src/components/ClaudeRecipe/ClaudeRecipe.tsx
@@ -1,5 +1,14 @@
+import ReactMarkdown from "react-markdown";
+
+import "./ClaudeRecipe.css";
+
 const ClaudeRecipe = (props: { recipe: string }) => {
-  return <section>{props.recipe}</section>;
+  return (
+    <section aria-live="polite" className="suggested-recipe-container">
+      <h2> Chef Claud Recommends:</h2>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
+    </section>
+  );
 };
 
 export default ClaudeRecipe;
