63a503b5fadb37d8272b062fb7d9f3d5bf2df9af
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-10 01:50:51 +0400

    Added NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./routes/Home";
 import Recipes from "./routes/Recipes";
 import Application from "./routes/Application";
+import NotFound from "./routes/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Application />}>
           <Route index element={<Home />} />
           <Route path="/recipes" element={<Recipes />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
