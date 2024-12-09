0af3fd6476d46fb9bab314b3c327beb2f1b4787f
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-09 22:27:37 +0400

    Added '/' for recipes route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="recipes" element={<Recipes />} />
+        <Route path="/recipes" element={<Recipes />} />
       </Routes>
     </>
   );
