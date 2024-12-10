8ee3181210f9d9d7819cf1037fb46730984fde20
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-10 21:39:02 +0400

    Firebase: - Created utils directory and added firebase.utils.tsx file - Added corresponding configurations - Created signInWithGoogle in the Firebase console - Mapped the Firebase instance with code - Updated Sign in component

--- src/routes/SignIn/SignIn.tsx
+++ src/routes/SignIn/SignIn.tsx
@@ -1,9 +1,17 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 import "./SignIn.css";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
   return (
     <div>
-      <h1>Sign In</h1>
+      <h1>Sign in</h1>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.utils.tsx
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-gm-B1REnT4x34zX_2EL25lLC1Rb_2xM",
+  authDomain: "recipes-app-f6787.firebaseapp.com",
+  projectId: "recipes-app-f6787",
+  storageBucket: "recipes-app-f6787.firebasestorage.app",
+  messagingSenderId: "12601005823",
+  appId: "1:12601005823:web:a3dc816d3297bb76661eca",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
