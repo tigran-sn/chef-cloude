47d823149a95fddf88230e1f92f2f1a0b6b30f90
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-12-13 21:07:02 +0400

    Changed the logo to use Logo component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
 import Navigation from "../Navigation";
 
-import logo from "../../assets/culinary-ai-logo.svg";
-
 import "./Header.css";
-// import Logo from "../Logo";
+import Logo from "../Logo";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__left">
         <Link to="/" className="header__logo-container">
-          {/* <Logo /> */}
-          <img src={logo} alt="Logo" className="header__logo" />
+          <Logo height={50} />
         </Link>
       </div>
       <div className="header__right">
